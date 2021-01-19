3622b6d6b4049226401fb9577093e702edf386c1
M4ND33P-M4L4K4R <70493026+M4ND33P-M4L4K4R@users.noreply.github.com>
2021-01-19 23:24:29 +0530

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,22 @@
+apt update
+apt upgrade -y
+apt install python -y
+apt install python2 -y
+apt install neofetch -y
+apt install cmatrix -y
+apt install htop -y
+apt install cacafire -y 
+apt install sl -y
+apt install ruby -y 
+gem install lolcat -y 
+clear
+echo
+echo
+echo
+echo -e "           Setup Successfully....    " | lolcat
+sleep 3
+clear
+echo -e "         \033[33m  Restarting......"
+sleep 2
+chmod +x script.sh
+bash script.sh
